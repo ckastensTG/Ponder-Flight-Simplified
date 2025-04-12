f896ed1200fb891f97c0251f74f490154d15e995
ckastensTG <christopher@timegalleon.com>
2025-04-12 16:58:45 -0300

    Basic tech tree implementation
    Two lines: size and engine type

--- LocFiles/CompNames_EN.lua
+++ LocFiles/CompNames_EN.lua
@@ -1,7 +1,7 @@
 -- read once and load into global arrays with information on which have been enabled
 return {
 	planeType = {"Piston","Turboprop","Turbojet","Turbofan"},
-    planeCat = {"Small (utility)","Medium (regional)","Large (narrow-body)","Mega (wide-body)", "Supersonic"},
+    planeCat = {"Small (utility)","Medium (regional)","Large (narrow-body)","Mega (wide-body)"},
     planeConfig = {"Passenger","Cargo"},
     fuelType = {"Gasoline", "Kerosene", "Biomass"},
 }
